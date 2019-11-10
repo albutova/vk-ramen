cf427e8eaf385a0a124643390e4770aa158bcdef
lena <albytova.elena@mail.ru>
2019-11-10 19:41:30 +0300

    Updates

---
+++ precache-manifest.388306e5f3bce2e5a98fd63b00f9fa35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e95ad76548526afbea7ac4a719089e",
+    "url": "/vk-ramen/index.html"
+  },
+  {
+    "revision": "e9b70d65ac368af5df98",
+    "url": "/vk-ramen/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "3155965b115f92dc3a8b",
+    "url": "/vk-ramen/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "e9b70d65ac368af5df98",
+    "url": "/vk-ramen/static/js/2.6d980ec8.chunk.js"
+  },
+  {
+    "revision": "3155965b115f92dc3a8b",
+    "url": "/vk-ramen/static/js/main.ef1200ce.chunk.js"
+  },
+  {
+    "revision": "7e497343a7383096bdb4",
+    "url": "/vk-ramen/static/js/runtime-main.bad6aa2b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-ramen/static/media/persik.4e1ec840.png"
+  }
+]);
